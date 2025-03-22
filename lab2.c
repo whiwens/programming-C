d611ed973b2418106880d51dd13bcdfd181672ed
whiwens <avojtenko5@gmail.com>
2025-03-22 11:38:16 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
     int rows, cols;
 
-    
+    srand(time(NULL));
+
     printf("Vvedite kolichestvo strok i stolbcov matricy (naprimer, 3 3): ");
     scanf("%d %d", &rows, &cols);
-
 
     double arr[rows][cols];
     double x1 = 0, x2 = 0;
 
-    
-    printf("Vvedite elementi massiva %dx%d:\n", rows, cols);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("Vvedite A[%d][%d]: ", i, j);
-            scanf("%lf", &arr[i][j]);
-        }
-    }
-
     printf("\nMassiv %dx%d:\n", rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
+            arr[i][j] = (double)(rand() % 100) / 10;
             printf("%6.2lf ", arr[i][j]);
         }
         printf("\n");
@@ -44,18 +38,14 @@ int main() {
 
     int arr2[rows][cols], arr3[rows][cols];
 
+    printf("\nMassiv %dx%d:\n", rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
+            arr2[i][j] = rand() % 100; 
             arr3[i][j] = 0;
+            printf("%4d ", arr2[i][j]);
         }
-    }
-
-    printf("Vvedite elementi massiva %dx%d:\n", rows, cols);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("Vvedite B[%d][%d]: ", i, j);
-            scanf("%d", &arr2[i][j]);
-        }
+        printf("\n");
     }
 
     if (rows == cols) {
