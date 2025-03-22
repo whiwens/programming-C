4c7407dfd9a88b5810b2277c16440df15a8f351d
whiwens <avojtenko5@gmail.com>
2025-03-22 11:18:48 +1000

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -1,57 +1,81 @@
 #include <stdio.h>
 
 int main() {
+    int rows, cols;
+
     
-    double arr[3][3];
+    printf("Vvedite kolichestvo strok i stolbcov matricy (naprimer, 3 3): ");
+    scanf("%d %d", &rows, &cols);
+
+
+    double arr[rows][cols];
     double x1 = 0, x2 = 0;
 
-    printf("Vvedite elementi massiva 3x3:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    
+    printf("Vvedite elementi massiva %dx%d:\n", rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("Vvedite A[%d][%d]: ", i, j);
             scanf("%lf", &arr[i][j]);
         }
     }
 
-    printf("\nMassiv 3x3:\n");
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    printf("\nMassiv %dx%d:\n", rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("%6.2lf ", arr[i][j]);
         }
         printf("\n");
     }
 
-    for (int i = 0; i < 3; i++) {
-        x1 += arr[i][i]; 
-        x2 += arr[i][2 - i]; 
+    if (rows == cols) {
+        for (int i = 0; i < rows; i++) {
+            x1 += arr[i][i]; 
+            x2 += arr[i][rows - 1 - i]; 
+        }
+        printf("\nSumma glavnoy diagonali (X1): %.2lf\n", x1);
+        printf("Summa pobochnoy diagonali (X2): %.2lf\n", x2);
+    } else {
+        printf("\nMatrica ne kvadratnaya, summy diagonaley ne vychislyayutsya.\n");
     }
-    printf("\nSumma glavnoy diagonali (X1): %.2lf\n", x1);
-    printf("Summa pobochnoy diagonali (X2): %.2lf\n", x2);
 
-    int arr2[2][2], arr3[2][2] = {0};
+    printf("\nVvedite kolichestvo strok i stolbcov vtoroy matricy (naprimer, 2 2): ");
+    scanf("%d %d", &rows, &cols);
 
-    printf("\nVvedite elementi massiva 2x2:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
+    int arr2[rows][cols], arr3[rows][cols];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arr3[i][j] = 0;
+        }
+    }
+
+    printf("Vvedite elementi massiva %dx%d:\n", rows, cols);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("Vvedite B[%d][%d]: ", i, j);
             scanf("%d", &arr2[i][j]);
         }
     }
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            for (int k = 0; k < 2; k++) {
-                arr3[i][j] += arr2[i][k] * arr2[k][j];
+    if (rows == cols) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < cols; k++) {
+                    arr3[i][j] += arr2[i][k] * arr2[k][j];
+                }
             }
         }
-    }
 
-    printf("\nKvadrat matricy 2x2:\n");
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            printf("%4d ", arr3[i][j]);
+        printf("\nKvadrat matricy %dx%d:\n", rows, cols);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                printf("%4d ", arr3[i][j]);
+            }
+            printf("\n");
         }
-        printf("\n");
+    } else {
+        printf("\nMatrica ne kvadratnaya, umnozhenie nevozmozhno.\n");
     }
 
     return 0;
