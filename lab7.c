86e7e4cd06cb09e1dd82cf4016fca8e9c98ffc85
whiwens <avojtenko5@gmail.com>
2025-05-10 10:46:27 +1000

    Update lab7.c

--- lab7.c
+++ lab7.c
@@ -1,26 +1,51 @@
- 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-struct humen{
-    char name[100], sname[100], pol[10];
-    int byear, height;  
+// Структура для хранения информации о человеке
+struct humen {
+    char name[50];      // Имя
+    char surname[50];   // Фамилия
+    int birthYear;      // Год рождения
 };
 
-int main(){
-    int N = 4;
-    struct humen a[N], b[N], t;
-    char name[100], sname[100], pol[10];
-    int byear, height;
-    for(int i = 0; i<N; i++){
-        printf("введите имя %d-го человека: ", i+1);
-        scanf("%s", name);
-        printf("введите фамилию %d-го человека: ", i+1);
-        scanf("%s", sname);
-        printf("введите год рождения %d-го человека: ", i+1);
-        scanf("%d", &byear);
-        printf("введите пол %d-го человека: ", i+1);
-        scanf
+// Функция для сравнения людей по году рождения (для qsort)
+int compareByBirthYear(const void *a, const void *b) {
+    const struct humen *h1 = (const struct humen *)a;
+    const struct humen *h2 = (const struct humen *)b;
+    return h1->birthYear - h2->birthYear;
+}
+
+int main() {
+    const int SIZE = 6;          // Количество людей
+    struct humen people[SIZE];   // Массив для данных
+
+    // Открываем файл для чтения
+    FILE *inputFile = fopen("input.txt", "r");
+    if (inputFile == NULL) {
+        printf("Ошибка открытия файла!\n");
+        return 1;
     }
+
+    // Читаем данные из файла
+    for (int i = 0; i < SIZE; ++i) {
+        fscanf(inputFile, "%s %s %d", 
+               people[i].name, 
+               people[i].surname, 
+               &people[i].birthYear);
+    }
+    fclose(inputFile);
+
+    // Сортируем массив по году рождения
+    qsort(people, SIZE, sizeof(struct humen), compareByBirthYear);
+
+    // Выводим отсортированные данные
+    for (int i = 0; i < SIZE; ++i) {
+        printf("%s %s %d\n", 
+               people[i].name, 
+               people[i].surname, 
+               people[i].birthYear);
+    }
+
     return 0;
 }
