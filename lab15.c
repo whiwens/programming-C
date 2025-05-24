a42976f4b50e151923beb4d4d38914bfe9efc43d
whiwens <avojtenko5@gmail.com>
2025-05-24 13:32:20 +1000

    Update lab15.c

--- lab15.c
+++ lab15.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 
-// Функция для подсчета количества цифр в числе
+// Функция проверки, является ли число нулем (включая 0000)
+int is_zero(int n) {
+    return n == 0;
+}
+
+// Функция подсчета количества цифр в числе
 int count_digits(int n) {
-    if (n < 10) return 1;
-    return 1 + count_digits(n / 10);
+    if (n == 0) return 1;
+    int count = 0;
+    while (n != 0) {
+        n /= 10;
+        count++;
+    }
+    return count;
 }
 
-// Функция для вывода цифр в прямом порядке
+// Функция вывода цифр в прямом порядке
 void print_forward(int n) {
-    if (n < 10) {
-        printf("%d ", n);
-        return;
+    int num_digits = count_digits(n);
+    for (int i = num_digits - 1; i >= 0; i--) {
+        int digit = (n / (int)pow(10, i)) % 10;
+        printf("%d ", digit);
     }
-    int power = pow(10, count_digits(n) - 1);
-    printf("%d ", n / power);
-    print_forward(n % power);
 }
 
-// Функция для вывода цифр в обратном порядке
+// Функция вывода цифр в обратном порядке
 void print_reverse(int n) {
-    if (n < 10) {
-        printf("%d ", n);
-        return;
+    while (n != 0) {
+        printf("%d ", n % 10);
+        n /= 10;
     }
-    printf("%d ", n % 10);
-    print_reverse(n / 10);
 }
 
 int main() {
     int N;
-    printf("Введите натуральное число N: ");
+    printf("Введите число: ");
     scanf("%d", &N);
+    
+    if (is_zero(N)) {
+        return 0;  // Завершаем программу без вывода
+    }
     
     printf("Цифры в прямом порядке: ");
     print_forward(N);
